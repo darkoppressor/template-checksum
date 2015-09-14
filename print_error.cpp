83c55f581bcad785f691eee624dccff97da6ae5c
Kevin Wells <darkoppressor@gmail.com>
2015-09-14 01:54:10 -0500

    Print error messages to standard output

--- print_error.cpp
+++ print_error.cpp
@@ -5,12 +5,16 @@
 #include "print_error.h"
 #include "file_io.h"
 
+#include <iostream>
+
 using namespace std;
 
 void print_error(string error_message){
     File_IO file_io;
 
     string error_msg="Error: "+error_message+"\n";
+
+    cout<<error_msg;
 
     file_io.save_file("log-checksum",error_msg,true);
 }
